7e40fc4b2ef21e6d398f9f361f2960a57419da9b
Zakjanzi <zakjanzi97@gmail.com>
2023-01-01 20:19:25 +0200

    added intersectionObserver for animations

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,15 +1,30 @@
 import React from 'react';
-
+import { useRef, useEffect, useState } from 'react';
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 // Constants separates the logic from the content.
 import { projects } from '../../constants/constants';
 
-const Projects = () => (
+
+
+
+
+export default function Projects() {
+  const myRef = useRef();
+  const [myElementIsVisible, setMyElementIsVisible] = useState();
+  console.log('myElementIsVisible', myElementIsVisible )
+  useEffect (() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      setMyElementIsVisible(entry.isIntersecting)
+    })
+    observer.observe(myRef.current)
+  }, [])
+  return (
   <Section nopadding id="projects">
     <SectionDivider />
     <br />
-    <SectionTitle>Projects</SectionTitle>
+    <SectionTitle ref={myRef}>Projects</SectionTitle>
     <GridContainer>
       {projects.map((project) => (
         <BlogCard key={project.id}>
@@ -35,6 +50,4 @@ const Projects = () => (
     </GridContainer>
   </Section>
 
-);
-
-export default Projects;
+)};

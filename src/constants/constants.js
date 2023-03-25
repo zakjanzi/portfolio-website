0ab7cbb2ea74afd1355d7fc9196a0a14521bd704
Zakjanzi <zakjanzi97@gmail.com>
2023-03-25 11:38:39 +0200

    content edits

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -35,15 +35,15 @@ export const projects = [
     live: 'https://zak-coinmarketcapclone.netlify.app/',
     id: 3,
   },
-  {
-    title: 'Metamask Login Button',
-    description: "A dApp login button using the Metamask API.",
-    image: '/images/5.PNG',
-    tags: ['🍦JavaScript', 'Metamask API'],
-    code: 'https://github.com/zakjanzi/metamask_login',
-    live: 'https://zak-connectwalletbutton.netlify.app/',
-    id: 4,
-  },
+  // {
+  //   title: 'Metamask Login Button',
+  //   description: "A dApp login button using the Metamask API.",
+  //   image: '/images/5.PNG',
+  //   tags: ['🍦JavaScript', 'Metamask API'],
+  //   code: 'https://github.com/zakjanzi/metamask_login',
+  //   live: 'https://zak-connectwalletbutton.netlify.app/',
+  //   id: 4,
+  // },
 ];
 
 // export const TimeLineData = [
